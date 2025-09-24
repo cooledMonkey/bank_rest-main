840347f72d7a4a6c298fbb3a8b58bac65abb0d4c
Cooled Monkey <lemonacidic@mail.ru>
2025-09-24 15:51:55 +0300

    first commit

---
+++ src/main/java/com/example/bankcards/BankRestMain.java
@@ -0,0 +1,11 @@
+package com.example.bankcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRestMain {
+    public static void main(String args[]){
+        SpringApplication.run(BankRestMain.class, args);
+    }
+}
